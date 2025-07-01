93b4fa0c4c6e78165c6ccf1282db83ef8a7d6838
victoriaplummer <victoria.plummer@webflow.com>
2025-07-01 12:48:40 -0400

    chore: updated base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/llms",
+  base: "/llmstxt",
   site: "http://localhost:4321",
   trailingSlash: "ignore",
   prefetch: true,
