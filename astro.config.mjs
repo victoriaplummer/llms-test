804769c267d550398c758ae12fbebabdf39b1eb2
victoriaplummer <victoria.plummer@webflow.com>
2025-07-03 16:57:19 -0400

    docs: updated css configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
